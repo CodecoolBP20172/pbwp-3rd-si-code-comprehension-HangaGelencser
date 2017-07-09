1305242330ea36a3f52ad995e22cc844529536bb
HangaGelencser <hangagelencser@gmail.com>
2017-07-09 23:17:56 +0200

    Add files via upload

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,35 @@
-import random
+#Guessing numbers
 
-guessesTaken = 0
+import random #Imports random module from library
 
-print('Hello! What is your name?')
-myName = input()
+guessesTaken = 0 # Assign value(0) to variable
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print('Hello! What is your name?') # prints the question in the breq	brackets
+myName = input() # Assign input to variable (myName)
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+number = random.randint(1, 20) # Returns a random integer between 1-20
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.') # Prints a sentence, with inserted variable (myName)
 
-    guessesTaken += 1
+while guessesTaken < 6: # While loop executes the target statement as long as guessesTaken < 6 is True
+    print('Take a guess.') # Prints a sentence
+    guess = input() # Asking input for a variable
+    guess = int(guess) # Converts guess variables value from a string to an integer
 
-    if guess < number:
-        print('Your guess is too low.')
+    guessesTaken += 1 # Adds one to guessesTaken variable, while changing the variable itself in process
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess < number: # Using if condition to compare if guess variable is smaller than number
+        print('Your guess is too low.') # If the condition is True, it prints a sentence reflectted on the actual condition
 
-    if guess == number:
-        break
+    if guess > number: # Using if condition again to decide if guess variable is bigger than number
+        print('Your guess is too high.') # If the condition is True, it prints a sentence reflectted on the actual condition
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess == number: # Using if condition with equal operation
+        break # If this condition is True, then it breaks out from the innermost, enclosing while loop
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess == number: # With if condition it checks, if guess variable and number variable is equal
+    guessesTaken = str(guessesTaken) # If the condition is True, it convert guessesTaken variable to string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!') # Prints a sentence reflected on the main task, inserting myName variable and guessesTaken variable
+
+if guess != number: # With if condition it checks, if guess variable is not equal to number variable
+    number = str(number) # If True, it converts number to a string
+    print('Nope. The number I was thinking of was ' + number) # Prints a sentence reflected on it, with number variable added to it
